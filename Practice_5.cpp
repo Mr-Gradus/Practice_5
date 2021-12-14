43c7fec6068e08cc038071f8db66b1c2d4ec5cc8
Mr-Gradus <rybin.dmitriy@gmail.com>
2021-12-14 23:32:37 +0500

    Task 1 is done
    const и указатели  долго додумывал где ставить ))

--- Practice_5.cpp
+++ Practice_5.cpp
@@ -14,10 +14,10 @@ private:
     T m_num2;
 
 public:
-    Pair1(T num1, T num2) : m_num1(num1), m_num2(num2) {};
+    Pair1(const T& num1, const T& num2) : m_num1(num1), m_num2(num2) {};
 	
-    T first() { return m_num1; }
-    T second() { return m_num2; }
+    const T& first() const { return m_num1; }
+    const T& second() const { return m_num2; }
     
     ~Pair1() {};
 
@@ -26,11 +26,13 @@ public:
 
 int main()
 {
-    Pair1<int> p1(6, 9);
-    cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+    cout << "#################### TASK_1 ####################" << endl << endl;
+  
+  Pair1<int> p1(6, 9);
+  cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
 
-   const Pair1<double> p2(3.4, 7.8);
-   cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+  const Pair1<double> p2(3.4, 7.8);
+  cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
 
     return 0;
 }
